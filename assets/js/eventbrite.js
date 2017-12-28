8c6cad4ebf65645527ff66de4bfdd37c8f347d73
Sean Kelly <spkellydev@gmail.com>
2017-12-28 18:51:27 -0500

    dynamic querying of the events endpoint on button click => replace with form

--- assets/js/eventbrite.js
+++ assets/js/eventbrite.js
@@ -7,18 +7,18 @@ let cats = {}
 let catVal
 var elem = document.getElementById('root');
 
-function handleResponse(res) {
+function handleCategoriesEndpointResponse(res) {
     let categories = res.categories
 
     document.querySelector('#root').innerHTML = '';
     categories.forEach( category  => {
 
-        cats[category.id] = category.name
+        cats[category.id] = category.short_name
     });
     return cats
 }
 
-function loadEBApi() {
+function loadEBApi(url) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true)
     xhr.onprogress = function () {
@@ -29,19 +29,25 @@ function loadEBApi() {
         status = this.status
         if ((status >= 200 && status < 300) || status == 304) {
             res = JSON.parse(this.responseText)
-            handleResponse(res)
-            
 
-            Object.keys(cats).forEach(function(key) {
+            if (endpoint == 'categories') {
+                handleCategoriesEndpointResponse(res)
+    
+                Object.keys(cats).forEach(function(key) {
+    
+                    let opt = document.createElement("OPTION")
+                    opt.value = key
+                    opt.innerHTML = cats[key]
+                    elem.appendChild(opt)
+                  
+                  });
+    
+                  catVal = elem.value
+                  loader.innerHTML = ''
+            } else {
+                console.log(res)
+            }
 
-                let opt = document.createElement("OPTION")
-                opt.value = key
-                opt.innerHTML = cats[key]
-                elem.appendChild(opt)
-              
-              });
-              catVal = elem.value
-              loader.innerHTML = ''
             
         } else {
             loader.innerHTML = 'Not Found'
@@ -55,10 +61,18 @@ function loadEBApi() {
     xhr.send();
 }
 
-loadEBApi();
+loadEBApi(url);
 
 
 
 elem.addEventListener('change', function() {
-    console.log(elem.value)
+    catVal = elem.value
 }, false);
+
+document.querySelector('#butt').addEventListener('click', function () {
+    endpoint = 'events/search'
+    url = `https://www.eventbriteapi.com/v3/${endpoint}/?token=${token}`
+    url += "&start_date.range_start=2017-12-31T19:00:00&start_date.range_end=2018-01-31T19:00:00&location.latitude=40.329555&location.longitude=-74.061529&categories=" + catVal + "&location.within=20mi"
+    console.log(url)
+    loadEBApi(url)
+})
