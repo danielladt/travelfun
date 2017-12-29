f90b9f8549681effac1ed88ab3f298e0f2a887b6
Sean Kelly <spkellydev@gmail.com>
2017-12-29 11:49:02 -0500

    Commenting my code and adding in animations

--- assets/js/eventbrite.js
+++ assets/js/eventbrite.js
@@ -9,6 +9,12 @@ let catVal
 var elem = document.getElementById('root');
 let results = document.getElementById('results')
 
+/**
+ * @param {*} res type object
+ * @return {*} cats type object
+ * @description Takes the json response from the EventBrite Categories API and creates an object with the category name and the category ID so that we may query the Events endpoint with a category ID while supplying the user a category name
+ * @see https://www.eventbrite.com/developer/v3/endpoints/categories/
+ */
 function handleCategoriesEndpointResponse(res) {
     let categories = res.categories
 
@@ -20,6 +26,14 @@ function handleCategoriesEndpointResponse(res) {
     return cats
 }
 
+/**
+ * 
+ * @param {res} type object
+ * @return {envs} type object, see @prop
+ * @prop {name: "name.text", description: "description.text", url : "url" time: {start: "start.utc", end: ...}, capacity, img} assumes event
+ * @description Takes the json response from the EventBrite Events API and creates a nested object struct separated by event ID
+ * @see https://www.eventbrite.com/developer/v3/endpoints/events/
+ */
 function handleEventsEndpointResponse(res) {
     let events = res.events
 
@@ -47,21 +61,39 @@ function handleEventsEndpointResponse(res) {
     return envs
 }
 
+/**
+ * 
+ * @param {string} url
+ * @requires endpoint, token
+ * @returns output for HTML
+ * @description Handles, prepares, and executes XMLHttpRequest to the EventBrite API. Looks for endpoint to be set before entering function and passed into the url param. By default, a loader will populate the area of the viewport until the onload function has handled its request
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequestEventTarget/onload
+ */
 function loadEBApi(url) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true)
+
+    /**
+     * 
+     * @param {*} e event
+     * @prop {e} assumes request has been sent
+     * @see ./assets/css/style.css
+     */
     xhr.onprogress = function (e) {
         document.body.className = "loading"
     }
 
     xhr.onload = function () {
+        //use the xhr object property status to check the result of the request
         status = this.status
         if ((status >= 200 && status < 300) || status == 304) {
+            //status was good, parse into JSON
             res = JSON.parse(this.responseText)
 
             if (endpoint == 'categories') {
                 handleCategoriesEndpointResponse(res)
     
+                //take the cats {categories} ID and name and load into select options
                 Object.keys(cats).forEach(function(key) {
     
                     let opt = document.createElement("OPTION")
@@ -70,22 +102,28 @@ function loadEBApi(url) {
                     elem.appendChild(opt)
                   
                   });
-    
+                  //set a default category value
                   catVal = elem.value
+                  //remove the loader created by onload function
                   document.body.className = ""
             } else {
+                //if categories is not the endpoint, then we are in the events endpoint
+                //reset the previous {events} object envs
+                envs = {}
+                results.innerHTML = ''
                 handleEventsEndpointResponse(res)
 
-                results.innerHTML = ''
-
-                console.log(envs)
-
+                //take the envs {events} and output them to the screen
                 Object.keys(envs).forEach(function(key) {
 
+                    //count used internally, not for user
                     if (key === 'count') {
                         return
                     }
 
+                    /**
+                     * @see https://bulma.io/documentation/components/card/
+                     */
                     let output = `
                     <div class="column is-3">
                     <div class="card">
@@ -110,24 +148,25 @@ function loadEBApi(url) {
                     </div></div>
                     `
 
-                    // let li = document.createElement('LI')
-                    // li.innerHTML = `<h3>${envs[key].name}</h3>`
-                    // li.dataset.id = key
-                    // results.appendChild(li)
-
+                    //if there has been an issue with output, no time to handle errors
                     if (!output) {
                         return
                     }
 
                     results.innerHTML += output
                 });
+                //hide the results and use the loader for default of 0.5s
                 results.style.display = 'none'
                 setTimeout(function () {
                     document.body.className = ""
                     results.style.display = 'inline-flex'
                     results.style.flexWrap = 'wrap'
                     
-                    //greensock to stagger the lis
+                    /**
+                     * @name TimelineLite
+                     * @see https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TimelineLite.min.js
+                     * @description makes javascript triggered CSS animations easier
+                     */
                     let tl = new TimelineLite()
                     tl.staggerFrom(results.children, 0.3, {
                         y: -15,
@@ -143,6 +182,7 @@ function loadEBApi(url) {
         }
     }
 
+    //need better error reporting in future
     xhr.onerror = function () {
         console.log('Request Error')
     }
@@ -158,11 +198,10 @@ elem.addEventListener('change', function() {
     catVal = elem.value
 }, false);
 
+//wait for event to trigger
 document.querySelector('#butt').addEventListener('click', function () {
     endpoint = 'events/search'
     url = `https://www.eventbriteapi.com/v3/${endpoint}/?token=${token}`
     url += "&start_date.range_start=2017-12-31T19:00:00&start_date.range_end=2018-01-31T19:00:00&location.latitude=40.329555&location.longitude=-74.061529&categories=" + catVal + "&location.within=5mi"
-    console.log(url)
-    results.innerHTML = ""
     loadEBApi(url)
 })
