a11ad9f76b5651a1958bad3833a4bfe5211d652b
Divya Ayikkara <divyaayikkara@Divyas-MacBook-Air.local>
2018-01-02 18:23:36 -0500

    done with geocoding

--- assets/js/googleplaces.js
+++ assets/js/googleplaces.js
@@ -1,7 +1,15 @@
 // userInput id needs to be changed!
-    var map;
-    var service;
-    var infowindow;
+
+//variables
+var map;
+var service;
+var infoWindow
+var geocoder; 
+var address; 
+var service;
+var latitude;
+var longitude;
+var place = [];
 
   // turning the user input into address for google
     function converter() {
@@ -11,14 +19,18 @@
     console.log(address);
   }
 
-  //google map
+  
     function initMap() {
+        //google map
       	var map = new google.maps.Map(document.getElementById("map"),{
           zoom:15,
           center: {lat: 42.3601, lng: -71.0589}
       	});
         var geocoder = new google.maps.Geocoder();
 
+        var address = document.getElementById("userInput").value;
+
+        //event listener 
         document.getElementById("butt").addEventListener("click", function(){
           geocodeAddress(geocoder, map);
         });
@@ -26,8 +38,10 @@
         // autocomplete
         var input = document.getElementById('userInput');
         var autocomplete = new google.maps.places.Autocomplete(input);
-      
 
+
+      
+      // locating the address on the map
       function geocodeAddress(geocoder, resultsMap) {
         var address = document.getElementById("userInput").value;
         geocoder.geocode({'address': address}, function(results, status) {
@@ -36,13 +50,26 @@
             var marker = new google.maps.Marker({
               map: resultsMap,
               position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+              });
+            //console.log(resultsMap);
+            var latitude = results[0].geometry.location.lat();;
+            var longitude = results[0].geometry.location.lng();;
+            console.log(latitude);
+            console.log(longitude);    
           }
-        });
+        })
       }
-    }
+  }
+
+
+
+
+        
+    
+
+
+
+
   
 
     
